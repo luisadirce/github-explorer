04faeb9ae90bf61e35d6a2064f26ba818828d73e
Luisa Dirce <luisa.dirce.silva@gmail.com>
2021-05-27 16:38:19 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/RepositoryList';
+import styles from './styles/global.scss';
+
+export function App() {
+	return (
+		<>
+			<RepositoryList />
+		</>
+	)
+}
